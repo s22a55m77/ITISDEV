01d26ebc370bef6eaa377485e324859e47951062
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-06-27 16:29:08 +0800

    Add Announcement Module

---
+++ views/announcementModule/announcementList/announcementList.js
@@ -0,0 +1,16 @@
+function popup() {
+  var popup = document.getElementById("popup");
+  popup.classList.toggle("show");
+}
+
+function deleteAnnouncement() {
+  console.log("clicked");
+}
+
+function editAnnouncement() {
+  console.log("clicked");
+}
+
+function create() {
+  console.log("created");
+}
