588e58c1ccfcdaa4b07288e143c3c6a79dd06814
s22a55m77 <s22a55m77@gmail.com>
2024-06-27 12:35:07 +0800

    initialize firebase sdk

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const e = require('express')
 
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const { initializeApp, cert } = require('firebase-admin/app')
+
+require('dotenv').config()
+
+initializeApp({
+  credential: cert({
+    type: 'service_account',
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  }),
+})
 
 const app = e()
 
