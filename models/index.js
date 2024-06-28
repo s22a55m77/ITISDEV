381e42efe7dcc15018a8db01756dc7eeb8f8618b
s22a55m77 <s22a55m77@gmail.com>
2024-06-28 14:35:26 +0800

    add user model

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const userModel = require('./userModel.js')
+
+module.exports = {
+  userModel,
+}
---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const { Schema } = require('mongoose')
+
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    designation: { type: String },
+    idNumber: { type: Number },
+    college: { type: String },
+    department: { type: String },
+    campus: { type: String },
+    eaf: { type: Buffer },
+    vaccinationCard: { type: Buffer },
+    role: {
+      type: String,
+      required: true,
+      enum: ['admin', 'client', 'dispatcher'],
+      default: 'client',
+    },
+  },
+  { collection: 'User', timestamps: true }
+)
+
+const userModel = mongoose.model('User', userSchema)
+
+module.exports = userModel
