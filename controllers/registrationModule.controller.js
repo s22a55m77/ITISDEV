f24f009f5fe3904e571a5a62e73d442061334d71
s22a55m77 <s22a55m77@gmail.com>
2024-06-28 15:18:05 +0800

    Add Firebase authentication

--- controllers/registrationModule.controller.js
+++ controllers/registrationModule.controller.js
@@ -1,4 +1,6 @@
 const e = require('express')
+const { getAuth } = require('firebase-admin/auth')
+const { userModel } = require('../models/index.js')
 
 const registrationModuleController = e.Router()
 
@@ -6,12 +8,38 @@ registrationModuleController.get('/signin', (req, res) => {
   res.render('registrationModule/signin.ejs')
 })
 
+registrationModuleController.post('/signin', async (req, res) => {
+  const { idToken } = req.body
+
+  const decodedToken = await getAuth().verifyIdToken(idToken)
+
+  if (!decodedToken.email.endsWith('@dlsu.edu.ph')) {
+    await getAuth().deleteUser(uid)
+    res.redirect('/auth/error?error=invalid-email-domain')
+  }
+
+  const user = await userModel.findOne({ email: decodedToken.email })
+
+  if (!user) {
+    res.redirect('/auth/confirm/create')
+  }
+  res.redirect('/auth/confirm')
+})
+
 registrationModuleController.get('/confirm', (req, res) => {
   res.render('registrationModule/confirm.ejs')
 })
 
-registrationModuleController.get('/success', (req, res) => {
+registrationModuleController.get('/success', async (req, res) => {
   const type = req.query.type
+
+  const sessionCookie = await getAuth().createSessionCookie(req.body.idToken, {
+    expiresIn: 60 * 60 * 24 * 30,
+  })
+
+  res.cookie('session', sessionCookie, {
+    maxAge: 60 * 60 * 24 * 30 * 1000,
+  })
 
   res.render('registrationModule/success.ejs', { type })
 })
