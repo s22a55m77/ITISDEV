f2a6d0f89a3407aeb9b7049a647704b16c5af47e
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-06-29 09:55:51 +0800

    Separate js into new file

---
+++ public/registrationModule/createAcc/createAcc.js
@@ -0,0 +1,8 @@
+// get idToken from query
+const urlParams = new URLSearchParams(window.location.search)
+const idToken = urlParams.get('idToken')
+
+// set idToken in hidden input
+$(document).ready(() => {
+  $('input[name="idToken"]').val(idToken)
+})
