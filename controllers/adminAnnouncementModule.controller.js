23776134a8128ed54cdca9068e41a525c64a9620
s22a55m77 <s22a55m77@gmail.com>
2024-06-29 13:30:46 +0800

    simplify folder structure

--- controllers/adminAnnouncementModule.controller.js
+++ controllers/adminAnnouncementModule.controller.js
@@ -3,31 +3,23 @@ const e = require('express')
 const adminAnnouncementModuleController = e.Router()
 
 adminAnnouncementModuleController.get('/', (req, res) => {
-  res.render(
-    'adminAnnouncementModule/adminAnnouncementList/adminAnnouncementList.ejs'
-  )
+  res.render('adminAnnouncementModule/adminAnnouncementList.ejs')
 })
 
 adminAnnouncementModuleController.get('/detail/:id', (req, res) => {
   const id = req.params.id
 
-  res.render(
-    'adminAnnouncementModule/adminAnnouncementDetail/adminAnnouncementDetail.ejs'
-  )
+  res.render('adminAnnouncementModule/adminAnnouncementDetail.ejs')
 })
 
 adminAnnouncementModuleController.get('/create', (req, res) => {
-  res.render(
-    'adminAnnouncementModule/adminCreateAnnouncement/adminCreateAnnouncement.ejs'
-  )
+  res.render('adminAnnouncementModule/adminCreateAnnouncement.ejs')
 })
 
 adminAnnouncementModuleController.get('/edit/:id', (req, res) => {
   const id = req.params.id
 
-  res.render(
-    'adminAnnouncementModule/adminEditAnnouncement/adminEditAnnouncement.ejs'
-  )
+  res.render('adminAnnouncementModule/adminEditAnnouncement.ejs')
 })
 
 module.exports = adminAnnouncementModuleController
