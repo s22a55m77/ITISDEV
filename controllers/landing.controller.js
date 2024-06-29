90dce8988a86e855a86e02b83da829ceff051bf8
s22a55m77 <s22a55m77@gmail.com>
2024-06-29 15:59:21 +0800

    add unread badge

--- controllers/landing.controller.js
+++ controllers/landing.controller.js
@@ -1,10 +1,18 @@
 const e = require('express')
+const httpContext = require('express-http-context')
 const isAuthorized = require('../utils/isAuthorized.js')
+const { announcementModel, userModel } = require('../models/index.js')
 
 const landingController = e.Router()
 
-landingController.get('/', isAuthorized, (req, res) => {
-  res.render('landing/landing.ejs')
+landingController.get('/', isAuthorized, async (req, res) => {
+  const email = httpContext.get('userEmail')
+
+  const user = await userModel.findOne({ email })
+
+  const unRead = await announcementModel.find({ read: { $ne: user._id } })
+
+  res.render('landing/landing.ejs', { unRead: unRead.length })
 })
 
 module.exports = landingController
