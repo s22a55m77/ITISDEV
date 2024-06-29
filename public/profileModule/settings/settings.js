f7fea396e6ceff258919942859bfa0ad8b8b84aa
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-06-29 14:14:00 +0800

    Add success, error message

--- public/profileModule/settings/settings.js
+++ public/profileModule/settings/settings.js
@@ -1,3 +1,4 @@
+// handle file upload
 $('#eaf-upload').on('change', function() {
   $('#eaf-upload-form').submit();
 })
@@ -7,6 +8,7 @@ $('#vaccination-upload').on('change', function() {
 })
 
 
+// handle date formatting
 const eafDate = new Date(eafTime);
 const options = { month: 'long', day: '2-digit' };
 const eafFormattedDate = eafDate.toLocaleDateString('en-US', options);
@@ -19,4 +21,28 @@ if(eafFormattedDate) {
 
 if(vaccinationFormattedDate) {
   $('#vaccination-updated-time').text("Last update " + vaccinationFormattedDate)
+}
+
+// get the query
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+let { success, error } = params; 
+
+if(success) {
+  $('#alert').css('background-color', '#00A14A')
+  $('#alert').text("Updated")
+  $('#alert').show();
+  setTimeout(() => {
+    $('#alert').hide();
+  }, 2000);
+}
+
+if(error) {
+  $('#alert').css('background-color', '#A70000')
+  $('#alert').text("Upload Failed")
+  $('#alert').show();
+  setTimeout(() => {
+    $('#alert').hide();
+  }, 2000);
 }
