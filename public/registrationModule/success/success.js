3d04a96d4174cf7f11d31e47fbfe9fd63d67d5e9
s13a46m79 <86817023+s13a46m79@users.noreply.github.com>
2024-06-29 12:45:22 +0800

    Delete console log

--- public/registrationModule/success/success.js
+++ public/registrationModule/success/success.js
@@ -2,7 +2,6 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 let value = params.type; 
-console.log(value);
 
 if (value === 'create') {
   $('#header-message').text('Create your account');
