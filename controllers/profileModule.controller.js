457adee9f06b122395574954c746bbdfc19204ea
s22a55m77 <s22a55m77@gmail.com>
2024-06-29 13:15:27 +0800

    function to update eaf and vaccination

--- controllers/profileModule.controller.js
+++ controllers/profileModule.controller.js
@@ -1,4 +1,5 @@
 const e = require('express')
+const moment = require('moment-timezone')
 const isAuthorized = require('../utils/isAuthorized')
 const { userModel } = require('../models/index.js')
 const httpContext = require('express-http-context')
@@ -19,8 +20,56 @@ profileModuleController.get('/', isAuthorized, async (req, res) => {
   })
 })
 
-profileModuleController.get('/settings', isAuthorized, (req, res) => {
-  res.render('profileModule/settings.ejs')
+profileModuleController.get('/settings', isAuthorized, async (req, res) => {
+  const email = httpContext.get('userEmail')
+  const { eafUpdatedAt, vaccinationRecordUpdatedAt } = await userModel.findOne({
+    email: email,
+  })
+
+  res.render('profileModule/settings.ejs', {
+    eafUpdatedAt,
+    vaccinationRecordUpdatedAt,
+  })
+})
+
+profileModuleController.post('/eaf', isAuthorized, async (req, res) => {
+  const email = httpContext.get('userEmail')
+
+  try {
+    const eafData = req.files.eaf.data
+    console.log(req.files.eaf.name)
+    await userModel.updateOne(
+      { email: email },
+      { eaf: eafData, eafUpdatedAt: moment().tz('Asia/Manila').format() }
+    )
+    res.redirect('/profile/settings?success=eaf-uploaded')
+    return
+  } catch (error) {
+    console.error(error)
+    res.redirect('/profile/settings?error=eaf-upload-error')
+    return
+  }
+})
+
+profileModuleController.post('/vaccination', isAuthorized, async (req, res) => {
+  const email = httpContext.get('userEmail')
+
+  try {
+    const vaccination = req.files.vaccination.data
+    await userModel.updateOne(
+      { email: email },
+      {
+        vaccinationRecord: vaccination,
+        vaccinationRecordUpdatedAt: moment().tz('Asia/Manila').format(),
+      }
+    )
+    res.redirect('/profile/settings?success=vaccination-uploaded')
+    return
+  } catch (error) {
+    console.error(error)
+    res.redirect('/profile/settings?error=vaccination-upload-error')
+    return
+  }
 })
 
 profileModuleController.get('/test', async (req, res) => {
