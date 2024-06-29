d7860adb12ec3c849977d088773b1535413761a1
s22a55m77 <s22a55m77@gmail.com>
2024-06-29 12:54:13 +0800

    get user profile data

--- controllers/profileModule.controller.js
+++ controllers/profileModule.controller.js
@@ -1,13 +1,33 @@
 const e = require('express')
+const isAuthorized = require('../utils/isAuthorized')
+const { userModel } = require('../models/index.js')
+const httpContext = require('express-http-context')
 
 const profileModuleController = e.Router()
 
-profileModuleController.get('/', (req, res) => {
-  res.render('profileModule/profile.ejs')
+profileModuleController.get('/', isAuthorized, async (req, res) => {
+  const email = httpContext.get('userEmail')
+  const { name, idNumber, picture, designation } = await userModel.findOne({
+    email: email,
+  })
+
+  res.render('profileModule/profile.ejs', {
+    name,
+    idNumber,
+    picture,
+    designation,
+  })
 })
 
-profileModuleController.get('/settings', (req, res) => {
+profileModuleController.get('/settings', isAuthorized, (req, res) => {
   res.render('profileModule/settings.ejs')
+})
+
+profileModuleController.get('/test', async (req, res) => {
+  const user = await userModel.findOne({
+    email: 'mathew_benavidez@dlsu.edu.ph',
+  })
+  res.send(user)
 })
 
 module.exports = profileModuleController
