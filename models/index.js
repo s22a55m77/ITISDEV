d75fa5a50db13e4758b46ce5c44746dc74c6e620
s22a55m77 <s22a55m77@gmail.com>
2024-07-01 11:58:29 +0800

    add models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const userModel = require('./userModel.js')
 const announcementModel = require('./announcementModel.js')
+const scheduleModel = require('./scheduleModel.js')
+const scheduleDetailModel = require('./scheduleDetailModel.js')
 
 module.exports = {
   userModel,
   announcementModel,
+  scheduleModel,
+  scheduleDetailModel,
 }
