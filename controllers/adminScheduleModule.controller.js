ac8221ad57257db99e1ee7ec7c6292b125c1a9bd
s22a55m77 <s22a55m77@gmail.com>
2024-07-01 11:41:31 +0800

    add endpoint

--- controllers/adminScheduleModule.controller.js
+++ controllers/adminScheduleModule.controller.js
@@ -2,4 +2,18 @@ const e = require('express')
 
 const adminScheduleModuleController = e.Router()
 
+adminScheduleModuleController.get('/', (req, res) => {
+  res.render('adminScheduleModule/schedule.ejs')
+})
+
+adminScheduleModuleController.get('/create', (req, res) => {
+  res.render('adminScheduleModule/create.ejs')
+})
+
+adminScheduleModuleController.get('/edit/:id', (req, res) => {
+  const id = req.params.id
+
+  res.render('adminScheduleModule/edit.ejs')
+})
+
 module.exports = adminScheduleModuleController
