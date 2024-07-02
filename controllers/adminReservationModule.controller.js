89b0e839490fe135e8fc7bb8ac19897f62ac1ebb
s22a55m77 <s22a55m77@gmail.com>
2024-07-02 15:25:20 +0800

    add view reservation function

--- controllers/adminReservationModule.controller.js
+++ controllers/adminReservationModule.controller.js
@@ -1,9 +1,161 @@
 const e = require('express')
+const {
+  scheduleModel,
+  scheduleDetailModel,
+  reservationApprovalModel,
+} = require('../models/index.js')
+const moment = require('moment-timezone')
+const { findNearestAndSurrounding } = require('../utils/dateUtil.js')
 
 const adminReservationModuleController = e.Router()
 
-adminReservationModuleController.get('/', (req, res) => {
-  res.render('adminReservationModule/reservation.ejs')
+adminReservationModuleController.get('/test', async (req, res) => {
+  const today = moment.tz('Asia/Manila').format('YYYY-MM-DD')
+
+  const schedule = await scheduleModel.aggregate([
+    {
+      $lookup: {
+        from: 'ScheduleDetail',
+        localField: 'details',
+        foreignField: '_id',
+        as: 'details',
+      },
+    },
+    {
+      $match: {
+        line: 1,
+      },
+    },
+    {
+      $project: {
+        details: {
+          _id: true,
+          time: true,
+        },
+      },
+    },
+  ])
+
+  const days = []
+  schedule[0].details.forEach((detail) =>
+    days.push(moment(detail.time).tz('Asia/Manila').format('YYYY-MM-DD'))
+  )
+
+  const uniqueDays = [...new Set(days)]
+
+  const nearestAndSurroundingTimes = findNearestAndSurrounding(
+    uniqueDays,
+    today
+  )
+
+  res.json({ schedule, nearestAndSurroundingTimes })
+})
+
+adminReservationModuleController.get('/', async (req, res) => {
+  const { selectedDate, selectedTime, line } = req.query
+
+  const schedule = await scheduleModel.aggregate([
+    {
+      $lookup: {
+        from: 'ScheduleDetail',
+        localField: 'details',
+        foreignField: '_id',
+        as: 'details',
+      },
+    },
+    {
+      $match: {
+        line: 1,
+      },
+    },
+    {
+      $project: {
+        details: {
+          _id: true,
+          from: true,
+          to: true,
+          time: true,
+        },
+      },
+    },
+  ])
+
+  const days = []
+  schedule[0].details.forEach((detail) =>
+    days.push(moment(detail.time).tz('Asia/Manila').format('YYYY-MM-DD'))
+  )
+
+  const uniqueDays = [...new Set(days)]
+
+  if (!line && !selectedDate) {
+    const today = moment.tz('Asia/Manila').format('YYYY-MM-DD')
+
+    const nearestAndSurroundingTimes = findNearestAndSurrounding(
+      uniqueDays,
+      today
+    )
+
+    // const firstDate
+
+    res.redirect(
+      `/admin/reservation?line=1&selectedDate=${nearestAndSurroundingTimes[2]}`
+    )
+    return
+  }
+
+  const nearestAndSurroundingTimes = findNearestAndSurrounding(
+    uniqueDays,
+    selectedDate
+  )
+
+  const details = schedule[0].details.filter(
+    (detail) =>
+      moment(detail.time).tz('Asia/Manila').format('YYYY-MM-DD') ===
+      selectedDate
+  )
+
+  const map = new Map()
+  details.forEach((detail) => {
+    const existing = map.get(detail.from)
+    const obj = {
+      id: detail._id,
+      time: moment(detail.time).tz('Asia/Manila').format('HH:mm'),
+    }
+    map.set(detail.from, existing ? existing.concat(obj) : [obj])
+  })
+
+  const timeList = []
+
+  for (const [key, value] of map.entries()) {
+    timeList.push(value)
+  }
+
+  let passengerList
+
+  if (selectedTime) {
+    passengerList = (
+      await scheduleDetailModel.findById(selectedTime).populate({
+        path: 'approval',
+        populate: {
+          path: 'user',
+        },
+      })
+    ).approval.map((approval) => {
+      return {
+        name: approval.user.name,
+        id: approval.user.idNumber,
+        designation: approval.designation,
+        purpose: approval.purpose,
+        status: approval.status,
+      }
+    })
+  }
+
+  res.render('adminReservationModule/reservation.ejs', {
+    dateList: JSON.stringify(nearestAndSurroundingTimes),
+    timeList: JSON.stringify(timeList),
+    passengerList: JSON.stringify(passengerList),
+  })
 })
 
 module.exports = adminReservationModuleController
--- public/adminReservationModule/reservation.js
+++ public/adminReservationModule/reservation.js
@@ -1,42 +1,34 @@
 import { lineName } from '../../lineInformation.js'
 
-const dateList = [
-  '2024-06-29',
-  '2024-06-30',
-  '2024-07-01',
-  '2024-07-02',
-  '2024-07-03',
-]
-
 const passengerList = [
   {
     name: 'Parcia, John Ronn',
     id: '12136859',
     designation: 'no LAG classes',
     purpose: 'campus visit',
-    status: 'pending'
+    status: 'pending',
   },
   {
     name: 'Parcia, John Ronn',
     id: '12136859',
     designation: 'no LAG classes',
     purpose: 'laboratory visit',
-    status: 'confirmed'
+    status: 'confirmed',
   },
   {
     name: 'Parcia, John Ronn',
     id: '12136859',
     designation: 'no LAG classes',
     purpose: 'laboratory visit',
-    status: 'rejected'
+    status: 'rejected',
   },
 ]
 
 // get the query
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
-});
-let { line, selectedDate, selectedTime } = params; 
+})
+let { line, selectedDate, selectedTime } = params
 
 // If no line information
 if (!line) {
@@ -51,34 +43,36 @@ $('#to-location-name').text(toFrom)
 
 // Filter popup
 function popup() {
-  var popup = document.getElementById("popup");
-  popup.classList.toggle("show");
+  var popup = document.getElementById('popup')
+  popup.classList.toggle('show')
 }
 
 // Set Date list
-const dateListElement = dateList.map(date => {
-  let className;
+const dateListElement = dateList.map((date) => {
+  let className
   const position = dateList.indexOf(selectedDate)
 
   // Set style
   if (date === selectedDate) {
     className = 'main-select'
-  } else if (dateList[position - 1] === date || dateList[position + 1] === date) {
+  } else if (
+    dateList[position - 1] === date ||
+    dateList[position + 1] === date
+  ) {
     className = 'sub-select'
   } else {
     className = 'not-select'
   }
 
-  return (
-    `<div class='${className}'>${moment(date).format('MMM DD').replace(/^(\D*)0/, '$1')}</div>`
-  )
+  return `<div class='${className}'>${moment(date)
+    .format('MMM DD')
+    .replace(/^(\D*)0/, '$1')}</div>`
 })
 
 $('#date-list').append(dateListElement)
 
-
 // Select Row
-let selectedData = {};
+let selectedData = {}
 function renderPassengerList(passengerList) {
   $('#passenger-list-table').empty()
   $('#passenger-list-table').append(`
@@ -89,9 +83,8 @@ function renderPassengerList(passengerList) {
       <th>Purpose</th>
     </tr>
   `)
-  const passengerListElement = passengerList.map(passenger => {
-    return (
-      `
+  const passengerListElement = passengerList.map((passenger) => {
+    return `
         <tr class="${passenger.status}">
           <td>${passenger.name}</td>
           <td>${passenger.id}</td>
@@ -99,23 +92,21 @@ function renderPassengerList(passengerList) {
           <td>${passenger.purpose}</td>
         </tr>
       `
-    )
   })
 
   $('#passenger-list-table').append(passengerListElement)
 
   // Select Row Hightlight
-  $('#passenger-list-table tr').on('click' ,function() {
+  $('#passenger-list-table tr').on('click', function () {
     // Make sure it is not th
-    if($(this).find('th').length > 0)
-      return;
-    
+    if ($(this).find('th').length > 0) return
+
     $('#passenger-list-table tr').removeClass('selected-row')
     $(this).toggleClass('selected-row')
-     selectedData.name = $(this).find('td').eq(0).text();
-     selectedData.id = $(this).find('td').eq(1).text();
-     selectedData.designation = $(this).find('td').eq(2).text();
-     selectedData.purpose = $(this).find('td').eq(3).text();
+    selectedData.name = $(this).find('td').eq(0).text()
+    selectedData.id = $(this).find('td').eq(1).text()
+    selectedData.designation = $(this).find('td').eq(2).text()
+    selectedData.purpose = $(this).find('td').eq(3).text()
   })
 }
 
@@ -123,46 +114,52 @@ function renderPassengerList(passengerList) {
 renderPassengerList(passengerList)
 
 // Filter
-let filter = ['pending', 'confirmed', 'rejected'];
-$('#confirmed').click(function(e) {
-  const isChecked = $(e.target).prop('checked');
+let filter = ['pending', 'confirmed', 'rejected']
+$('#confirmed').click(function (e) {
+  const isChecked = $(e.target).prop('checked')
   if (isChecked) {
-    filter.push('confirmed');
+    filter.push('confirmed')
   } else {
-    filter = filter.filter(status => status !== 'confirmed')
+    filter = filter.filter((status) => status !== 'confirmed')
   }
-  const filteredPassengerList = passengerList.filter(passenger => filter.includes(passenger.status));
+  const filteredPassengerList = passengerList.filter((passenger) =>
+    filter.includes(passenger.status)
+  )
   renderPassengerList(filteredPassengerList)
 })
 
-$('#pending').click(function(e) {
-  const isChecked = $(e.target).prop('checked');
+$('#pending').click(function (e) {
+  const isChecked = $(e.target).prop('checked')
   if (isChecked) {
-    filter.push('pending');
+    filter.push('pending')
   } else {
-    filter = filter.filter(status => status !== 'pending')
+    filter = filter.filter((status) => status !== 'pending')
   }
-  const filteredPassengerList = passengerList.filter(passenger => filter.includes(passenger.status));
+  const filteredPassengerList = passengerList.filter((passenger) =>
+    filter.includes(passenger.status)
+  )
   renderPassengerList(filteredPassengerList)
 })
 
-$('#rejected').click(function(e) {
-  const isChecked = $(e.target).prop('checked');
+$('#rejected').click(function (e) {
+  const isChecked = $(e.target).prop('checked')
   if (isChecked) {
-    filter.push('rejected');
+    filter.push('rejected')
   } else {
-    filter = filter.filter(status => status !== 'rejected')
+    filter = filter.filter((status) => status !== 'rejected')
   }
-  const filteredPassengerList = passengerList.filter(passenger => filter.includes(passenger.status));
+  const filteredPassengerList = passengerList.filter((passenger) =>
+    filter.includes(passenger.status)
+  )
   renderPassengerList(filteredPassengerList)
 })
 
 // Confirm Reservation
-$('#confirm').click(function() {
+$('#confirm').click(function () {
   console.log(selectedData)
 })
 
 // Reject Reservation
-$('#reject').click(function() {
+$('#reject').click(function () {
   console.log(selectedData)
 })
