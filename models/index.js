cf2d7645be1ef11b25cf9bad8a5233f4a5799ab4
s22a55m77 <s22a55m77@gmail.com>
2024-07-02 13:28:31 +0800

    add reservation approval model

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ const userModel = require('./userModel.js')
 const announcementModel = require('./announcementModel.js')
 const scheduleModel = require('./scheduleModel.js')
 const scheduleDetailModel = require('./scheduleDetailModel.js')
+const reservationApprovalModel = require('./reservationApprovalModel.js')
 
 module.exports = {
   userModel,
   announcementModel,
   scheduleModel,
   scheduleDetailModel,
+  reservationApprovalModel,
 }
