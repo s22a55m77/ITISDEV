0cfb3f17aaf138f6ebab02f94da68510c8b2fb0d
s22a55m77 <s22a55m77@gmail.com>
2024-07-06 12:55:43 +0800

    add endpoint to get date

--- controllers/reservationModule.controller.js
+++ controllers/reservationModule.controller.js
@@ -1,6 +1,50 @@
 const e = require('express')
+const { scheduleDetailModel } = require('../models/index.js')
+const moment = require('moment-timezone')
 
 const reservationModuleController = e.Router()
+
+reservationModuleController.get('/date', async (req, res) => {
+  const { from, to } = req.query
+
+  const schedule = await scheduleDetailModel.aggregate([
+    {
+      $match: {
+        from,
+        to,
+        time: {
+          $gte: new Date(),
+        },
+      },
+    },
+    {
+      $project: {
+        time: {
+          $dateToString: {
+            format: '%Y-%m-%d',
+            date: '$time',
+          },
+        },
+      },
+    },
+  ])
+
+  if (schedule.length === 0) return res.send({ success: false })
+
+  const weekdays = []
+  const saturdays = []
+
+  schedule.forEach((s) => {
+    const date = moment(s.time).tz('Asia/Taipei')
+    if (date.day() === 6) {
+      saturdays.push(s.time)
+    } else {
+      weekdays.push(s.time)
+    }
+  })
+
+  res.send({ weekdays, saturdays })
+})
 
 reservationModuleController.get('/', (req, res) => {
   res.render('reservationModule/reserveTrip.ejs')
