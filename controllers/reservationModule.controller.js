3bde8949358f52862048b2fd9acf9fdcbc2eb735
s22a55m77 <s22a55m77@gmail.com>
2024-07-06 13:56:10 +0800

    data for reservation

--- controllers/reservationModule.controller.js
+++ controllers/reservationModule.controller.js
@@ -4,6 +4,121 @@ const moment = require('moment-timezone')
 
 const reservationModuleController = e.Router()
 
+async function getCommonTime(from, to, date) {
+  const schedule = await scheduleDetailModel.aggregate([
+    {
+      $match: {
+        from,
+        to,
+      },
+    },
+    {
+      $project: {
+        slot: true,
+        time: true,
+        date: {
+          $dateToString: {
+            format: '%Y-%m-%d',
+            date: '$time',
+            timezone: 'Asia/Manila',
+          },
+        },
+      },
+    },
+    {
+      $match: {
+        date: {
+          $in: date,
+        },
+        slot: {
+          $gte: 1,
+        },
+      },
+    },
+    {
+      $project: {
+        slot: true,
+        time: {
+          $dateToString: {
+            format: '%H:%M',
+            date: '$time',
+            timezone: 'Asia/Manila',
+          },
+        },
+      },
+    },
+    {
+      $group: {
+        _id: '$time',
+        count: { $sum: 1 }, // Count occurrences of each time
+      },
+    },
+    {
+      $match: {
+        count: date.length,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        time: '$_id',
+      },
+    },
+  ])
+
+  if (schedule.length != 0) return schedule.map((s) => s.time)
+
+  return []
+}
+
+async function getSchedules(from, to, timeList) {
+  const schedules = await scheduleDetailModel.aggregate([
+    {
+      $match: {
+        from,
+        to,
+      },
+    },
+    {
+      $project: {
+        _id: true,
+        slot: true,
+        time: {
+          $dateToString: {
+            format: '%H:%M',
+            date: '$time',
+            timezone: 'Asia/Manila',
+          },
+        },
+      },
+    },
+    {
+      $match: {
+        time: {
+          $in: timeList,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: '$time',
+        slot: { $min: '$slot' },
+        sid: { $push: '$_id' },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        slot: true,
+        time: '$_id',
+        sid: true,
+      },
+    },
+  ])
+
+  return schedules.map((s) => ({ ids: s.sid, slot: s.slot, time: s.time }))
+}
+
 reservationModuleController.get('/date', async (req, res) => {
   const { from, to } = req.query
 
@@ -49,6 +164,40 @@ reservationModuleController.get('/date', async (req, res) => {
 
 reservationModuleController.get('/', (req, res) => {
   res.render('reservationModule/reserveTrip.ejs')
+})
+
+reservationModuleController.post('/departure', async (req, res) => {
+  const { date, from, to } = req.body
+
+  const timeList = await getCommonTime(from, to, date)
+
+  const schedules = await getSchedules(from, to, timeList)
+
+  /**
+   * [
+        {
+            "ids": [
+                "6688c5fa52edeaa2a2864630",
+                "6688d31e1229e21193bf63c3"
+            ],
+            "slot": 1,
+            "time": "12:20"
+        }
+    ]
+   * 
+   */
+
+  res.send({ schedules })
+})
+
+reservationModuleController.post('/return', async (req, res) => {
+  const { date, from, to } = req.body
+
+  const timeList = await getCommonTime(to, from, date)
+
+  const schedules = await getSchedules(to, from, timeList)
+
+  res.send({ schedules })
 })
 
 module.exports = reservationModuleController
