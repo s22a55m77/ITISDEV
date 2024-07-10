3e2e77eb9bdfd241de8f50288b2154af72623998
s22a55m77 <s22a55m77@gmail.com>
2024-07-10 11:13:28 +0800

    admin landing page authorization and data

--- controllers/adminLanding.controller.js
+++ controllers/adminLanding.controller.js
@@ -1,9 +1,21 @@
 const e = require('express')
+const isAuthorized = require('../utils/isAuthorized')
+const httpContext = require('express-http-context')
 
 const adminLandingController = e.Router()
 
-adminLandingController.get('/', (req, res) => {
-  res.render('adminLanding/landing.ejs')
+adminLandingController.get('/', isAuthorized, (req, res) => {
+  const user = httpContext.get('user')
+
+  if (
+    user.role !== 'admin' &&
+    user.role !== 'ssu' &&
+    user.role !== 'dispatcher'
+  ) {
+    res.redirect('/403.html')
+  }
+
+  res.render('adminLanding/landing.ejs', { name: user.name, role: user.role })
 })
 
 module.exports = adminLandingController
