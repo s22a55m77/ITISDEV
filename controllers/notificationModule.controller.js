d485adbd29139a67d48d1c091bab021fd586b6c9
s22a55m77 <s22a55m77@gmail.com>
2024-07-10 10:23:09 +0800

    pass notification to ejs

--- controllers/notificationModule.controller.js
+++ controllers/notificationModule.controller.js
@@ -1,13 +1,32 @@
 const e = require('express')
+const { notificationModel } = require('../models/index.js')
+const { ObjectId } = require('mongoose').Types
 
 const notificationModuleController = e.Router()
 
-notificationModuleController.get('/', (req, res) => {
-  res.render('notificationModule/notification.ejs')
+notificationModuleController.get('/', async (req, res) => {
+  const notifications = await notificationModel.find({
+    to: {
+      $in: [user._id],
+    },
+  })
+
+  res.render('notificationModule/notification.ejs', { notifications })
 })
 
-notificationModuleController.get('/detail', (req, res) => {
-  res.render('notificationModule/notificationDetail.ejs')
+notificationModuleController.get('/detail/:id', async (req, res) => {
+  const id = req.params.id
+
+  const notification = await notificationModel.find({
+    $and: {
+      to: {
+        $in: [user._id],
+      },
+      _id: new ObjectId(id),
+    },
+  })
+
+  res.render('notificationModule/notificationDetail.ejs', { notification })
 })
 
 module.exports = notificationModuleController
