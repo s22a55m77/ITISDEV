896d5c75fb3be9717d779173d05d13779f3e1799
s22a55m77 <s22a55m77@gmail.com>
2024-07-27 20:02:53 +0800

    add style for checkin

--- public/adminCheckInModule/checkIn/checkIn.js
+++ public/adminCheckInModule/checkIn/checkIn.js
@@ -1,37 +1,43 @@
-
-
 // Error Handling
-if(error) {
+if (error) {
   $('.information-container').empty()
   $('.information-container').append('No Schedule Found')
   $('.information-container').show()
 }
 
 // Redirect if no date selected
-if(!date && !error) {
+if (!date && !error) {
   window.location.href = '/admin/checkin?date=' + moment().format('YYYY-MM-DD')
 }
 // Set Date
-if(date)
-  $('#date').text(moment(date).format('MMMM DD, YYYY'))
-else 
-  $('#date').text(moment().format('MMMM DD, YYYY'))
-
+if (date) $('#date').text(moment(date).format('MMMM DD, YYYY'))
+else $('#date').text(moment().format('MMMM DD, YYYY'))
 
 // Only work when have data
-if(from && to && time) {
+if (from && to && time) {
   // Set location and time
   $('#from-location').val(from)
   $('#to-location').val(to)
-  $('#time-select').append(`<option value="${time}">${moment(time, 'HH:mm').format('hh:mm A')}</option>`)
+  $('#time-select').append(
+    `<option value="${time}">${moment(time, 'HH:mm').format(
+      'hh:mm A'
+    )}</option>`
+  )
   $('#time-select').val(time)
 
   // Show information
-  if(slotCount, reservedCount, presentCount, walkInCount) {
-    $('#reserved-seats').html(`<span class="bold">${reservedCount}</span> out of ${slotCount} `)
-    $('#currently-present').html(`<span class="bold">${presentCount}</span> out of ${reservedCount} `)
-    $('#walk-in').html(`<span class="bold">${walkInCount}</span> out of ${slotCount} `)
+  if ((slotCount, reservedCount, presentCount, walkInCount)) {
+    $('#reserved-seats').html(
+      `<span class="bold">${reservedCount}</span> out of ${slotCount} `
+    )
+    $('#currently-present').html(
+      `<span class="bold">${presentCount}</span> out of ${reservedCount} `
+    )
+    $('#walk-in').html(
+      `<span class="bold">${walkInCount}</span> out of ${slotCount} `
+    )
     $('.information-container').show()
+    $('.information-container').css('display', 'flex')
   }
 
   // Render Passenger List
@@ -40,34 +46,44 @@ if(from && to && time) {
     const passengerListElement = passengerList.map((passenger) => {
       return `
         <div class="passenger-item">
-          <div class="${passenger.status}"></div>
-          <div>${passenger.name}</div>
+          <div>
+            <div class="${passenger.status}"></div>
+            <div>${passenger.name}</div>
+          </div>
           <div>${passenger.id}</div>
         </div>
       `
     })
-    
+
     $('#passenger-list-container').append(passengerListElement)
   }
-  
+
   renderPassengerList(passengerList)
 
   // Handle no passenger
-  if(passengerList.length === 0) {
-    $('#passenger-list-container').append('<div class="no-passenger">No Reserved Passenger</div>')
+  if (passengerList.length === 0) {
+    $('#passenger-list-container').append(
+      '<div class="no-passenger">No Reserved Passenger</div>'
+    )
   }
 }
 
-if(passengerList.length === 0 && !from && !to && !time)
-  $('#passenger-list-container').append('<div class="no-passenger">Select a trip and time for this date to view list of reservations</div>')
+if (passengerList.length === 0 && !from && !to && !time)
+  $('#passenger-list-container').append(
+    '<div class="no-passenger">Select a trip and time for this date to view list of reservations</div>'
+  )
 
 // Handle change date
-function changeDate(action){
+function changeDate(action) {
   const dateNow = date ?? moment().format('YYYY-MM-DD')
-  if(action === 'prev')
-    window.location.href = `/admin/checkin?date=${moment(dateNow).subtract(1, 'days').format('YYYY-MM-DD')}`
-  else if(action === 'next')
-    window.location.href = `/admin/checkin?date=${moment(dateNow).add(1, 'days').format('YYYY-MM-DD')}`
+  if (action === 'prev')
+    window.location.href = `/admin/checkin?date=${moment(dateNow)
+      .subtract(1, 'days')
+      .format('YYYY-MM-DD')}`
+  else if (action === 'next')
+    window.location.href = `/admin/checkin?date=${moment(dateNow)
+      .add(1, 'days')
+      .format('YYYY-MM-DD')}`
 }
 
 function checkLocationIsSelected() {
@@ -82,19 +98,30 @@ function checkLocationIsSelected() {
 }
 
 async function handleLocationChange() {
-  if(checkLocationIsSelected()) {
-    const res = await fetch(`/admin/checkin/time?date=${moment().format('YYYY-MM-DD')}&from=${$('#from-location').val()}&to=${$('#to-location').val()}`)
-    .then(response => response.json())
-    
-    if(res.success) {
+  if (checkLocationIsSelected()) {
+    const res = await fetch(
+      `/admin/checkin/time?date=${moment(date).format('YYYY-MM-DD')}&from=${$(
+        '#from-location'
+      ).val()}&to=${$('#to-location').val()}`
+    ).then((response) => response.json())
+
+    if (res.success) {
       res.timeList.map((time) => {
         $('#time-select').empty()
-        $('#time-select').append(`<option disabled selected value>Select Time</option>`)
-        $('#time-select').append(`<option value="${time}">${ moment(time, 'HH:mm').format('hh:mm A')}</option>`)
+        $('#time-select').append(
+          `<option disabled selected value>Select Time</option>`
+        )
+        $('#time-select').append(
+          `<option value="${time}">${moment(time, 'HH:mm').format(
+            'hh:mm A'
+          )}</option>`
+        )
       })
-    }else {
+    } else {
       $('#time-select').empty()
-      $('#time-select').append(`<option disabled selected value>No Available Time</option>`)
+      $('#time-select').append(
+        `<option disabled selected value>No Available Time</option>`
+      )
     }
   }
 }
@@ -108,19 +135,25 @@ $('#to-location').change(() => {
 })
 
 $('#time-select').change(() => {
-  window.location.href = `/admin/checkin?date=${date}&from=${$('#from-location').val()}&to=${$('#to-location').val()}&time=${$('#time-select').val()}`
+  window.location.href = `/admin/checkin?date=${date}&from=${$(
+    '#from-location'
+  ).val()}&to=${$('#to-location').val()}&time=${$('#time-select').val()}`
 })
 
-$('#search').on('keyup', function() {
-  if($(this).val() === '' || $(this).val() === null) {
+$('#search').on('keyup', function () {
+  if ($(this).val() === '' || $(this).val() === null) {
     renderPassengerList(passengerList)
     return
   }
-  const result = passengerList.filter(passenger => passenger.name.includes($(this).val()) 
-                                                || passenger.id.includes($(this).val()))
+
+  const result = passengerList.filter(
+    (passenger) =>
+      passenger.name.toLowerCase().includes($(this).val().toLowerCase()) ||
+      passenger.id.toString().includes($(this).val())
+  )
   renderPassengerList(result)
 })
 
-$('#scan').click(function() {
+$('#scan').click(function () {
   window.location.href = `/admin/checkin/scan?from=${from}&to=${to}&time=${time}&date=${date}`
 })
