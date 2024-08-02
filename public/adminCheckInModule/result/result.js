ae7dd06c9f86017961d83f9788d38916d8563e8c
s22a55m77 <s22a55m77@gmail.com>
2024-08-02 09:56:56 +0800

    add style to checkin

--- public/adminCheckInModule/result/result.js
+++ public/adminCheckInModule/result/result.js
@@ -1,8 +1,21 @@
-const statusMessage = result === 'true' ? 'Marked present:': 'No reservation found for:'
+const statusMessage =
+  result === 'true' ? 'Marked present:' : 'No reservation found for:'
 $('#status-message').text(statusMessage)
 
-if(invalid === 'true') {
+if (result === 'true') {
+  $('#check').show()
+  $('#status-message').addClass('green')
+}
+
+if (result === 'false' && invalid === 'false') {
+  $('#exclamation').show()
+  $('#status-message').addClass('red')
+}
+
+if (invalid === 'true') {
   $('#status-message').text('Invalid QR code. Try again.')
+  $('#status-message').addClass('red')
+  $('#cross').show()
   $('#name').hide()
   $('#id').hide()
   $('#type').hide()
@@ -11,13 +24,13 @@ if(invalid === 'true') {
 // get query
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
-});
-let { time, to, from, date } = params;
+})
+let { time, to, from, date } = params
 
 $('#scan-another').click(() => {
-  window.location.href = `/admin/checkin/scan/?from=${from}&to=${to}&time=${time}&date=${date}`;
+  window.location.href = `/admin/checkin/scan/?from=${from}&to=${to}&time=${time}&date=${date}`
 })
 
 $('#back-to-list').click(() => {
-  window.location.href = `/admin/checkin?from=${from}&to=${to}&time=${time}&date=${date}`;
+  window.location.href = `/admin/checkin?from=${from}&to=${to}&time=${time}&date=${date}`
 })
